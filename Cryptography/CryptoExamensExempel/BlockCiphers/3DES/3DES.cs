374a8d970bf262a1d333de41f50ad60277a3f235
Morgan Hedberg <morganmauritzhedberg@gmail.com>
2022-05-22 13:22:07 +0200

    Init2

---
+++ Cryptography/CryptoExamensExempel/BlockCiphers/3DES/3DES.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoExamensExempel.BlockCiphers._3DES
+{
+    internal class _3DES
+    {
+    }
+}
